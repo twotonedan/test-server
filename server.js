353c496c1ea42f6359da960a66cfc3c8e44bb8f8
dansapp-stellar <dan.sapp@stellarims.com>
2023-10-28 17:40:52 -0500

    dockerize

--- server.js
+++ server.js
@@ -1,33 +1,7 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {   //create web server
-  if (req.url == '/') { //check the URL of the current request
-      
-      // set response header
-      res.writeHead(200, { 'Content-Type': 'text/html' }); 
-      
-      // set response content    
-      res.write('<html><body><p>This is home Page.</p></body></html>');
-      res.end();
-  
-  }
-  else if (req.url == "/student") {
-      
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<html><body><p>This is student Page.</p></body></html>');
-      res.end();
-  
-  }
-  else if (req.url == "/admin") {
-      
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<html><body><p>This is admin Page.</p></body></html>');
-      res.end();
-  
-  }
-  else
-      res.end('Invalid Request!');
+const ronin = require('ronin-server')
+const mocks = require('ronin-mocks')
 
-});
+const server = ronin.server()
 
-server.listen(5000); //6 - listen for any incoming requests
-console.log('Server running at http://127.0.0.1:1337/');
+server.use('/', mocks.server(server.Router(), false, true))
+server.start()
